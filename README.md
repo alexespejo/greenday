c25c6ea57bb2485a47391ec92e65b94893b7b97a
Alex Espejo <aespejo102@gmail.com>
2025-02-21 12:26:00 -0800

    dailly commit!

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # 🟩🟩🟩
 Fri Feb 21 11:49:51 PST 2025: Daily Commit!
 Fri Feb 21 12:18:52 PST 2025: Daily Commit!
+Fri Feb 21 12:26:00 PST 2025: Daily Commit!
---
+++ cronjob.py
@@ -0,0 +1,20 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from datetime import datetime
+import subprocess
+
+def my_task():
+    command = "zsh green.zsh"
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    print("Output:", result.stdout)
+    print("Error:", result.stderr)
+
+
+
+scheduler = BlockingScheduler()
+scheduler.add_job(my_task, 'cron', hour=12, minute=26)  # Runs every day at 12:25 PM
+
+try:
+    print("Scheduler started...")
+    scheduler.start()
+except (KeyboardInterrupt, SystemExit):
+    pass
