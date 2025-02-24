aebcfa6a1cf43c10d42fbd8892d165618fe4d891
Alex Espejo <aespejo102@gmail.com>
2025-02-23 19:17:20 -0800

    Daily commit!

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Fri Feb 21 12:18:52 PST 2025: Daily Commit!
 Fri Feb 21 12:26:00 PST 2025: Daily Commit!
 Sat Feb 22 21:30:09 PST 2025: Daily Commit!
 Sun Feb 23 13:51:44 PST 2025: Daily Commit!
+Sun Feb 23 19:17:20 PST 2025: Daily Commit!
--- test.sh
+++ test.sh
@@ -1,13 +1,24 @@
 flag=1
 echo "Starting!" 
 commit_changes() {
+    # Append the current date to README.md
+    echo "$(date): Daily Commit!" >> "README.md"
+
+    # Check if the file was updated successfully
+    if [ $? -eq 0 ]; then
+        echo "Content written to README.md successfully."
+    else
+        echo "Error writing to README.md"
+        return 1
+    fi
+
     # Check if the current directory is a Git repository
     if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
         # Add the file to Git
-        git add .
+        git add . 
 
         # Commit the changes
-        git commit -m "daily commit!"
+        git commit -m "Daily commit!"
 
         if [ $? -eq 0 ]; then
             echo "Changes committed successfully."
@@ -21,11 +32,11 @@ commit_changes() {
     fi
 }
 
+
 while true; do
-	if [[ "$(date +%H:%M)" == "19:14" ]]; then
+	if [[ "$(date +%H:%M)" == "19:17" ]]; then
 		if [ $flag -eq 1 ]; then
 				commit_changes
-
 				flag=0
 		fi 
 	else 
